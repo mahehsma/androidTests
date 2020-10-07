647cf8e77b2ce5374a9c96efc10dad75e5326b34
mahehsma <56866320+mahehsma@users.noreply.github.com>
2020-10-07 20:23:12 +0200

    first commit

---
+++ app/src/main/java/com/example/pathfinder/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.pathfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private String pathToThisElement = "element"; //rename to sdcard
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button_startPathFinder = findViewById(R.id.button_startPathFinder);
+        button_startPathFinder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchPath();
+            }
+        });
+    }
+
+    private void searchPath(){
+        Toast.makeText(getApplicationContext(),R.string.startingSearch, Toast.LENGTH_SHORT).show();
+        PathFinder pathFinder = new PathFinder();
+        String path = pathFinder.getPath(pathToThisElement);
+        setText(path);
+    }
+
+    private void setText(String path){
+        TextView textView_showPath = findViewById(R.id.textView_path);
+        if(path == null) textView_showPath.setText(R.string.pathNotFound);
+        else textView_showPath.setText(path);
+    }
+}
---
+++ app/src/main/java/com/example/pathfinder/PathFinder.java
@@ -0,0 +1,7 @@
+package com.example.pathfinder;
+
+public class PathFinder {
+    public String getPath(String element){
+        return null;
+    }
+}
